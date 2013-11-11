9e9636de3af6873af8e39ba9885b5718d6b42443
rpelyush <rpelyush@gmail.com>
2013-11-11 20:48:44 +0200

    new reader action spec

--- spec/controllers/readers_controller_spec.rb
+++ spec/controllers/readers_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe ReadersController do
+  describe 'GET new' do
+    let!(:reader) { mock_model('Reader').as_new_record }
 
+    before :each do
+      Reader.stub(:new).and_return(reader)
+      get :new
+    end
+
+    it 'assigns @reader variable to the view' do
+      expect(assigns[:reader]).to eq(reader)
+    end
+    it 'renders new template' do
+      expect(response).to render_template :new
+    end
+  end
 end
