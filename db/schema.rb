9d422fc8cf4404269e82ffe91feef1ba40cc6fec
rpelyush <rpelyush@gmail.com>
2013-11-15 23:57:01 +0200

    Login model spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111190148) do
+ActiveRecord::Schema.define(version: 20131115215130) do
+
+  create_table "logins", force: true do |t|
+    t.string "email"
+    t.string "password"
+  end
 
   create_table "readers", force: true do |t|
     t.string   "email"
