fb65ca5db96700f358b7efa692ddf66f2ee5660f
rpelyush <rpelyush@gmail.com>
2013-11-23 19:25:08 +0200

    books_controller spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe BooksController do
+  describe 'GET new' do
+    let!(:book) { mock_model('Book') }
 
+    before :each do
+      Book.stub(:new).and_return(book)
+      get :new
+    end
+
+    it 'assigns @book variable to the view' do
+      expect(assigns[:book]).to eq(book)
+    end
+
+    it 'renders new template' do
+      expect(response).to render_template :new
+    end
+  end
 end
